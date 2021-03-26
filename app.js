6edd7ae2e815adbc588708f85066a1649d63dfe8
Reshul <53306036+resel143@users.noreply.github.com>
2021-03-26 11:46:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+// let name = 'selena';
+// console.log(name);
+
+
+// //getting items from form and display it
+
+
+
+// document.querySelector('#btn').addEventListener('click', function(e){
+//    e.preventDefault();
+//     let startTime = document.getElementById('startTime').value;
+//     let endTime = document.getElementById('endTime').value;
+//     let todo = document.getElementById('event').value;
+//     let r =  confirm(`ToDo: ${todo} From ${startTime} - ${endTime}`);
+// //    console.log("Start Time "+startTime);
+// //    console.log("End Time "+endTime);
+// //    console.log("Event "+ todo);
+//     if(r){
+//         let ntr = document.createElement('tr');
+//         let blankset = document.createElement('td');
+//         let timeset = document.createElement('td');
+//         let jobset= document.createElement('td');
+//         let timesetText = document.createTextNode(`${startTime} - ${endTime}`);
+//         let jobsetText = document.createTextNode(`${todo}`);
+//         timeset.appendChild(timesetText);
+//         jobset.appendChild(jobsetText);
+//         ntr.appendChild(blankset);
+//         ntr.appendChild(timeset);
+//         ntr.appendChild(jobset);
+        
+//         document.querySelector('#outputTable').appendChild(ntr);
+//     }
+   
+
+// });
 let name = 'selena';
 console.log(name);
 
@@ -8,14 +43,17 @@ console.log(name);
 
 document.querySelector('#btn').addEventListener('click', function(e){
    e.preventDefault();
-    let startTime = document.getElementById('startTime').value;
+    
+   let startTime = document.getElementById('startTime').value;
     let endTime = document.getElementById('endTime').value;
     let todo = document.getElementById('event').value;
-    let r =  confirm(`ToDo: ${todo} From ${startTime} - ${endTime}`);
+    
 //    console.log("Start Time "+startTime);
 //    console.log("End Time "+endTime);
 //    console.log("Event "+ todo);
-    if(r){
+    if(startTime && endTime && todo)
+    { let r =  confirm(`ToDo: ${todo} From ${startTime} - ${endTime}`);
+        if(r){
         let ntr = document.createElement('tr');
         let blankset = document.createElement('td');
         let timeset = document.createElement('td');
@@ -30,6 +68,8 @@ document.querySelector('#btn').addEventListener('click', function(e){
         
         document.querySelector('#outputTable').appendChild(ntr);
     }
-   
+   }else{
+        alert('Please Enter all Criteria');
+   }
 
 });
